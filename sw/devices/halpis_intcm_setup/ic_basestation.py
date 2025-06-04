00314ae2e0f1195b86fc485232f186ec9151e8da
Niclas Hallgren <nicke@hallgren.fi>
2025-06-04 14:25:04 +0300

    Fixed a typo and a bug in last_interaction_time

--- sw/devices/halpis_intcm_setup/ic_basestation.py
+++ sw/devices/halpis_intcm_setup/ic_basestation.py
@@ -72,7 +72,7 @@ def wait_for_mumble(timeout=60, interval=1):
                             font_path=intercom.font_path,
                             font_size=intercom.small_font_size)
         render_text_display(index=3,
-                            text="START",
+                            text="TO START",
                             font_path=intercom.font_path,
                             font_size=intercom.small_font_size)
         sleep(interval)
@@ -1477,7 +1477,7 @@ if __name__ == "__main__":
                 # Display status on displays
                 sleep(1)
                 if not mumble_started:
-                  intercom.last_interaction_time = int(time() * 1000) + 125
+                  intercom.last_interaction_time = int((time() + 125) * 1000)
                   if wait_for_mumble(timeout=120):
                       mumble_started = True
                       sleep(5)
